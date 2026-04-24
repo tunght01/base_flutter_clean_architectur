25eac66f3fa71faa017dc496d2fb7001f49457c7
tunght01 <tung.hathanh.it@gmail.com>
2026-04-24 13:43:56 +0700

    test tich hop shorebird

--- lib/presentation/ui/authencation/login/widget/login_form.dart
+++ lib/presentation/ui/authencation/login/widget/login_form.dart
@@ -226,7 +226,7 @@ class _LoginFormState extends State<LoginForm>
                         Expanded(
                           child: AppButton(
                             isLoading: state.showLoginButtonLoading,
-                            label: S.current.create,
+                            label: S.current.login_button,
                             labelTextStyle: AppTextStyle.bold16(
                               color: Colors.white,
                             ),
